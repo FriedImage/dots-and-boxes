4e614fdda314902f86cf71536cf1e596e135c396
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-05-18 01:06:35 +0300

    Added App.java

---
+++ App.java
@@ -0,0 +1,39 @@
+ package gr.csihu.ntolias;
+
+// Imports
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+import javafx.scene.image.Image;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage menuStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("menuStage.fxml"));
+        Scene scene = new Scene(root);
+
+        // Stage settings (before loadfxml)
+        menuStage.setResizable(false);
+        Image icon = new Image("icon.png");
+        menuStage.getIcons().add(icon);
+        menuStage.setTitle("DnB: Main Menu");
+        menuStage.setScene(scene);
+        menuStage.show();
+    }
+
+    protected static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
