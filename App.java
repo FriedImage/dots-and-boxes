f672c054253286656298d5fbc5f757b5e61be68b
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-05-22 03:50:15 +0300

    Update App.java

--- App.java
+++ App.java
@@ -1,36 +1,29 @@
- package gr.projects.dotsandboxes;
+package com.test.jfxgame;
 
-// Imports
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
 import java.io.IOException;
 
-/**
- * JavaFX App
- */
+
 public class App extends Application {
 
+    private static Scene scene;
+
     @Override
-    public void start(Stage menuStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("menuStage.fxml"));
-        Scene scene = new Scene(root);
-
-        // Stage settings (before loadfxml)
-        menuStage.setResizable(false);
-        menuStage.setTitle("DnB: Main Menu");
-        menuStage.setScene(scene);
-        menuStage.show();
-    }
-
-    protected static Parent loadFXML(String fxml) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
-        return fxmlLoader.load();
+    public void start(Stage stage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Game.fxml"));
+        scene = new Scene(root);
+        stage.setTitle("game fx");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
