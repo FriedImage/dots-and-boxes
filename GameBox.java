fd54191420c9a9b4c10db03329e67c2e3463b77f
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-05-22 23:37:35 +0300

    Update GameBox.java

--- GameBox.java
+++ GameBox.java
@@ -1,5 +1,8 @@
 package com.test.jfxgame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GameBox {
 
     int column;
@@ -7,16 +10,20 @@ public class GameBox {
 
     boolean completed;
 
+    private List<GameLine> linesOfBox = new ArrayList<>();
+
     BoxOwner boxOwner;
 
-    enum BoxOwner {
-        PLAYER1, PLAYER2, NONE
-    }
+    GameLine line1, line2, line3, line4;
 
-//    GameBox(int column, int row, BoxOwner boxOwner, boolean completed) {
-//        this.column = column;
-//        this.row = row;
-//        this.boxOwner = boxOwner;
-//        this.completed = completed;
+//    enum BoxOwner {
+//        PLAYER1, PLAYER2, NONE
 //    }
+    GameBox(int column, int row, BoxOwner boxOwner, boolean completed, List<GameLine> linesOfBox) {
+        this.linesOfBox = linesOfBox.add(line1, line2, line3, line4);
+        this.column = column;
+        this.row = row;
+        this.boxOwner = (BoxOwner) BoxOwner.NONE;
+        this.completed = false;
+    }
 }
