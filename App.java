852ea9b31a3231a8d14062fa35d994397ac98c4a
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-05-22 23:36:44 +0300

    Update App.java

--- App.java
+++ App.java
@@ -23,7 +23,8 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch(args);
+//        launch(args);
+        new GameLogic().runGame();
     }
 
 }
