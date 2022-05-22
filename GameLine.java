480491b5dac829a00bfa4eb45fd4123eafcee368
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-05-22 23:38:19 +0300

    Update GameLine.java

--- GameLine.java
+++ GameLine.java
@@ -1,25 +1,21 @@
 package com.test.jfxgame;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class GameLine {
 
     int column;
     int row;
 
-    enum LineType {
-        HORZ, VERT
-    }
+//    enum LineType {
+//        HORZ, VERT
+//    }
+    LineType type;
 
     boolean activated;
-    
 
-
-//    GameLine(int column, int row, LineType lineType, boolean activated) {
-//        this.column = column;
-//        this.row = row;
-//        this.lineType = lineType;
-//        this.activated = activated;
-//    }
+    GameLine(int column, int row, LineType type, boolean activated) {
+        this.column = column;
+        this.row = row;
+        this.type = type;
+        this.activated = false;
+    }
 }
