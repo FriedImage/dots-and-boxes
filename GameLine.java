3c646cb37207c7252ae24824cdf00277b9e3e5b5
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-06-05 17:48:03 +0300

    Update GameLine.java

--- GameLine.java
+++ GameLine.java
@@ -7,18 +7,7 @@ public class GameLine {
 
     LineType type;
 
-    enum LineType {
-        HORZ, VERT
-    }
-
     boolean activated;
-
-    GameLine(int column, int row, LineType type, boolean activated) {
-        this.column = column;
-        this.row = row;
-        this.type = type;
-        this.activated = false;
-    }
 
     public boolean getActivatedProperty() {
         return activated;
@@ -27,4 +16,12 @@ public class GameLine {
     public void setActivatedProperty(boolean newActivated) {
         this.activated = newActivated;
     }
+
+    GameLine(int column, int row, LineType type) {
+        this.column = column;
+        this.row = row;
+        this.type = type;
+        this.activated = false;
+    }
+
 }
