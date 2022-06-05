e8fe6027b829118af6288e5b06c351eba88073b9
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-06-05 17:47:36 +0300

    Update GameBox.java

--- GameBox.java
+++ GameBox.java
@@ -8,26 +8,28 @@ public class GameBox {
     int column;
     int row;
 
+    BoxOwner boxOwner;
+
     boolean completed;
 
     private final List<GameLine> sides = new ArrayList<>();
 
-    private BoxOwner boxOwner;
-
-    private GameLine line1, line2, line3, line4;
-
-    private enum BoxOwner {
-        PLAYER1, PLAYER2, NONE
-    }
-
+    // moved the enum to the BoxOwner class
+//    private enum BoxOwner {
+//        PLAYER1, PLAYER2, NONE
+//    }
     public boolean isAtLocation(int column, int row) {
         return ((this.column == column) && (this.row == row));
     }
 
-    GameBox(GameLine line1, GameLine line2, GameLine line3, GameLine line4, List<GameLine> sides) {
-//        this.column = column;
-//        this.row = row;
-        this.sides = sides.addAll(line1, line2, line3, line4); // says I can't add stuff to final types which makes sense, so that's why I didn't make that list final.
+    public boolean isBoxComplete() {
+//        return sides.containsAll(gameLine.activated); - doesn't work
+        return sides.stream()
+                .allMatch(gameLine -> gameLine.activated);
+    }
+    
+    GameBox(GameLine line1, GameLine line2, GameLine line3, GameLine line4) {
+        sides.addAll(List.of(line1, line2, line3, line4));
         this.boxOwner = BoxOwner.NONE;
         this.completed = false;
     }
