b36988916ac19fd7885be6d9a569c6b4f8f9d465
FriedImage <91257240+FriedImage@users.noreply.github.com>
2022-06-05 17:48:21 +0300

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -1,67 +1,91 @@
 package com.test.jfxgame;
 
-import javafx.scene.shape.Box;
-
+//import javafx.scene.shape.Box;
 import java.util.ArrayList;
 import java.util.List;
 
 public class GameLogic {
 
-    private List<Box> boxs = new ArrayList<>();
-    private List<GameLine> gameLines = new ArrayList<>();
-    private List<GameLine> gameBoxs = new ArrayList<>();
+    private List<GameBox> boxs = new ArrayList<>();
+    private final List<GameLine> gameLines = new ArrayList<>();
+    private final List<GameLine> gameBoxs = new ArrayList<>();
+
+    BoxOwner activePlayer;
 
     void runGame() {
 
     }
 
-    GameLine findOrCreateGameLine(LineType type, int column, int row, boolean activated) {
-        GameLine gameLine = new GameLine(column, row, type);
-
-        gameLines.forEach(gameLine -> {
-            if ((type == type) && (gameLine.column == column) && (gameLine.row == row)) {
-                return gameLine;
-            }
-        });
-
-        gameLines.add(gameLine);
-        return gameLine;
-    }
-    
-    GameBox findOrCreateGameBox(LineType type, int column, int row, boolean activated) {
-        GameBox gameBox = new gameBox(column, row, type);
-
-        gameBoxs.forEach(gameBox -> {
-            if ((type == type) && (gameBox.column == column) && (gameBox.row == row)) {
-                return gameBox;
-            }
-        });
-
-        gameBoxs.add(gameBox);
-        return gameBox;
+    // new GameLine method
+    GameLine findOrCreateGameLine(LineType type, int column, int row) {
+        return gameLines.stream()
+                .filter((GameLine gameLine) -> ((gameLine.type.equals(type)) && (gameLine.column == column) && (gameLine.row == row)))
+                .findAny()
+                .orElseGet(() -> {
+                    GameLine gameLine = new GameLine(column, row, type);
+                    gameLines.add(gameLine);
+                    return gameLine;
+                });
     }
 
+    // something i've tried to do instead of streams ( doesn't work sadly )
+//    GameLine findOrCreateGameLine(LineType type, int column, int row) {
+//        return for(GameLine line : gameLines) {
+//            if ((line.type.equals(type)) && (line.column == column) && (line.row == row)) {
+//                gameLines.add(line);
+//            } else {
+//                GameLine line2 = new GameLine(column, row, type);
+//                gameLines.add(line2);
+//                return line2;
+//            }
+//        }
+//    }
+    // old GameLine method
+//        GameLine gameLine = new GameLine(column, row, type);
+//
+//        gameLines.forEach(gameLine -> {
+//            if ((type == type) && (gameLine.column == column) && (gameLine.row == row)) {
+//                return gameLine;
+//            }
+//        });
+//
+//        gameLines.add(gameLine);
+//        return gameLine;
     void populateBoard(int maxColumns, int maxRows) {
         for (int column = 0; column < maxColumns; column++) {
             for (int row = 0; row < maxRows; row++) {
-                GameLine topLine = findOrCreateGameLine(GameLine.type.HORZ, column, row);
-                GameLine bottomLine = findOrCreateGameLine(GameLine.LineType.HORZ, column, row + 1);
-                GameLine leftLine = findOrCreateGameLine(GameLine.LineType.VERT, column, row);
-                GameLine rightLine = findOrCreateGameLine(GameLine.LineType.VERT, column + 1, row);
+                GameLine topLine = findOrCreateGameLine(LineType.HORZ, column, row);
+                GameLine bottomLine = findOrCreateGameLine(LineType.HORZ, column, row + 1);
+                GameLine leftLine = findOrCreateGameLine(LineType.VERT, column, row);
+                GameLine rightLine = findOrCreateGameLine(LineType.VERT, column + 1, row);
 
-                boxs.add(new Box(topLine, bottomLine, leftLine, rightLine));
+                boxs.add(new GameBox(topLine, bottomLine, leftLine, rightLine));
             }
         }
     }
 
     void activateLine(LineType type, int column, int row, boolean activated) {
-        int startingCompleted = 0;
+        long startingCompleted = countCompletedBoxes();
 
-        findOrCreateGameLine(type, column, row, activated = true);
-        for (Box box : boxs) {
-            if boxs.get(box) { continue };
-            startingCompleted += 1;
+        GameLine gameLine = findOrCreateGameLine(type, column, row);
+        gameLine.activated = true;
+
+        if (countCompletedBoxes() == startingCompleted) {
+            flipCurrentPlayer();
         }
+    }
+
+    private void flipCurrentPlayer() {
+        if (this.activePlayer == BoxOwner.PLAYER1) {
+            activePlayer = BoxOwner.PLAYER2;
+        } else {
+            activePlayer = BoxOwner.PLAYER1;
+        }
+    }
+
+    private long countCompletedBoxes() {
+        return boxs.stream()
+                .filter(GameBox::isBoxComplete).count();
     }
 
 }
