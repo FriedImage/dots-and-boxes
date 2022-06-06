57dc0985e67661bdacb8f1829586cf9fc44c36e9
George <91257240+FriedImage@users.noreply.github.com>
2022-06-07 01:52:40 +0300

    Update GameBox.java

--- GameBox.java
+++ GameBox.java
@@ -14,20 +14,44 @@ public class GameBox {
 
     private final List<GameLine> sides = new ArrayList<>();
 
-    // moved the enum to the BoxOwner class
-//    private enum BoxOwner {
-//        PLAYER1, PLAYER2, NONE
-//    }
+    public int getColumn() {
+        for (GameLine line : sides) {
+            if (line.type.equals(LineType.HORZ)) {
+                return line.column;
+            }
+        }
+        return 0;
+    }
+    
+    public int getRow() {
+        for (GameLine line : sides) {
+            if (line.type.equals(LineType.VERT)) {
+                return line.row;
+            }
+        }
+        return 0;
+    }
+
     public boolean isAtLocation(int column, int row) {
         return ((this.column == column) && (this.row == row));
     }
 
+    // Box completion check ( stream-iteration version )
+//    public boolean isBoxComplete() {
+////        return sides.containsAll(gameLine.activated); - doesn't work
+//        return sides.stream()
+//                .allMatch(gameLine -> gameLine.activated);
+//    }
+    // Box completion check ( for-loop version )
     public boolean isBoxComplete() {
-//        return sides.containsAll(gameLine.activated); - doesn't work
-        return sides.stream()
-                .allMatch(gameLine -> gameLine.activated);
+        for (GameLine line : sides) {
+            if (!line.activated) {
+                return false;
+            }
+        }
+        return true;
     }
-    
+
     GameBox(GameLine line1, GameLine line2, GameLine line3, GameLine line4) {
         sides.addAll(List.of(line1, line2, line3, line4));
         this.boxOwner = BoxOwner.NONE;
