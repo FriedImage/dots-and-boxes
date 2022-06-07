2a7697358e66ee856647b91025f83e9c3e8b3d32
George <91257240+FriedImage@users.noreply.github.com>
2022-06-08 01:36:01 +0300

    Update GameController.java

--- GameController.java
+++ GameController.java
@@ -2,27 +2,9 @@ package com.test.jfxgame;
 
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.shape.Line;
 
 public class GameController implements Initializable {
-
-    @FXML
-    Line line1;
-
-    @FXML
-    Line line2;
-
-    @FXML
-    Line line3;
-
-    @FXML
-    Line line4;
-
-    
-
-
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
