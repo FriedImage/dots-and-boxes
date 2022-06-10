608483223b8e17231c156eec7412f76bb1667b9e
George <91257240+FriedImage@users.noreply.github.com>
2022-06-11 02:27:02 +0300

    Update GameBox.java

--- GameBox.java
+++ GameBox.java
@@ -2,7 +2,6 @@ package com.test.jfxgame;
 
 import java.util.ArrayList;
 import java.util.List;
-//import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
@@ -11,7 +10,6 @@ public class GameBox {
     int column;
     int row;
 
-//    BoxOwner boxOwner;
     final ObjectProperty<BoxOwner> boxOwner = new SimpleObjectProperty<>(BoxOwner.NONE);
 
     boolean completed;
@@ -26,7 +24,7 @@ public class GameBox {
         }
         return 0;
     }
-    
+
     public int getRow() {
         for (GameLine line : sides) {
             if (line.type.equals(LineType.VERT)) {
@@ -40,16 +38,9 @@ public class GameBox {
         return ((this.column == column) && (this.row == row));
     }
 
-    // Box completion check ( stream-iteration version )
-//    public boolean isBoxComplete() {
-////        return sides.containsAll(gameLine.activated); - doesn't work
-//        return sides.stream()
-//                .allMatch(gameLine -> gameLine.activated);
-//    }
     // Box completion check ( for-loop version )
     public boolean isBoxComplete() {
         for (GameLine line : sides) {
-//            if (!line.activated)
             if (!line.activated.get()) {
                 return false;
             }
@@ -59,7 +50,6 @@ public class GameBox {
 
     GameBox(GameLine line1, GameLine line2, GameLine line3, GameLine line4) {
         sides.addAll(List.of(line1, line2, line3, line4));
-//        this.boxOwner = BoxOwner.NONE;
         this.boxOwner.set(BoxOwner.NONE);
         this.completed = false;
     }
