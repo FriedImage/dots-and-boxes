6e1558c32a9cf58f24e4df60b1342943f77ad101
George <91257240+FriedImage@users.noreply.github.com>
2022-06-11 02:24:32 +0300

    Update GameLogic.java

--- GameLogic.java
+++ GameLogic.java
@@ -1,6 +1,5 @@
 package com.test.jfxgame;
 
-//import javafx.scene.shape.Box;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.beans.property.ObjectProperty;
@@ -12,14 +11,12 @@ public class GameLogic {
     final List<GameLine> gameLines = new ArrayList<>();
     private final List<GameLine> gameBoxs = new ArrayList<>();
 
-//    BoxOwner activePlayer;
     final ObjectProperty<BoxOwner> activePlayer = new SimpleObjectProperty<>(BoxOwner.NONE);
 
     void runGame() {
         // Setup Game
         populateBoard(2, 2);
 
-//        activePlayer = BoxOwner.PLAYER1;
         activePlayer.set(BoxOwner.PLAYER1);
         playLine(LineType.VERT, 0, 0);
         playLine(LineType.HORZ, 0, 0);
@@ -40,17 +37,6 @@ public class GameLogic {
 
     }
 
-    // GameLine method ( stream-iteration version )
-//    GameLine findOrCreateGameLine(LineType type, int column, int row) {
-//        return gameLines.stream()
-//                .filter((GameLine gameLine) -> ((gameLine.type.equals(type)) && (gameLine.column == column) && (gameLine.row == row)))
-//                .findAny()
-//                .orElseGet(() -> {
-//                    GameLine gameLine = new GameLine(column, row, type);
-//                    gameLines.add(gameLine);
-//                    return gameLine;
-//                });
-//    }
     // new GameLine method ( for-loop version )
     GameLine findOrCreateGameLine(LineType type, int column, int row) {
         for (GameLine line : gameLines) {
@@ -86,7 +72,6 @@ public class GameLogic {
         long startingCompleted = countCompletedBoxes();
 
         GameLine gameLine = findOrCreateGameLine(type, column, row);
-//        gameLine.activated = true;
         gameLine.activated.set(true);
         assignCompletedBoxes();
 
@@ -97,20 +82,16 @@ public class GameLogic {
 
     private void assignCompletedBoxes() {
         for (GameBox gameBox : boxs) {
-            if (gameBox.isBoxComplete() && (gameBox.boxOwner.get().equals(BoxOwner.NONE))) { // - equals problem
-//                gameBox.boxOwner = activePlayer;
-                gameBox.boxOwner.set(activePlayer.get()); // not sure if this is correct
+            if (gameBox.isBoxComplete() && (gameBox.boxOwner.get().equals(BoxOwner.NONE))) {
+                gameBox.boxOwner.set(activePlayer.get());
             }
         }
     }
 
     private void flipCurrentPlayer() {
-//        if (this.activePlayer.equals(BoxOwner.PLAYER1)) { // - equals problem
-        if (activePlayer.get().equals(BoxOwner.PLAYER1)) { // works ( .get() with .equals() )
-//            activePlayer = BoxOwner.PLAYER2;
+        if (activePlayer.get().equals(BoxOwner.PLAYER1)) {
             activePlayer.set(BoxOwner.PLAYER2);
         } else {
-//            activePlayer = BoxOwner.PLAYER1;
             activePlayer.set(BoxOwner.PLAYER1);
         }
     }
@@ -120,15 +101,4 @@ public class GameLogic {
         return boxs.stream()
                 .filter(GameBox::isBoxComplete).count();
     }
-
-    // count completed boxes ( for-loop version - doesn't work correctly )
-//    private long countCompletedBoxes() {
-//        long cnt = 0;
-//        for (GameBox gameBox : boxs) {
-//            if (gameBox.completed) {
-//                cnt += 1;
-//            }
-//        }
-//        return cnt;
-//    }
 }
