cef00dab64479fd3af76438dd6c8089dcc63f3d7
George <91257240+FriedImage@users.noreply.github.com>
2022-06-14 16:19:03 +0300

    Update GameData.java

--- GameData.java
+++ GameData.java
@@ -9,12 +9,13 @@ import javafx.beans.property.SimpleBooleanProperty;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleObjectProperty;
 
+// Every dynamic game data goes here ( moved from GameLogic )
 public class GameData {
 
     List<GameBox> boxes = new ArrayList<>();
     final List<GameLine> gameLines = new ArrayList<>();
 
-    final ObjectProperty<BoxOwner> activePlayer = new SimpleObjectProperty<>(BoxOwner.NONE);
+    final ObjectProperty<BoxOwner> activePlayer = new SimpleObjectProperty<>(BoxOwner.PLAYER1);
     final BooleanProperty gameOver = new SimpleBooleanProperty(false);
     IntegerProperty player1Score = new SimpleIntegerProperty(0);
     IntegerProperty player2Score = new SimpleIntegerProperty(0);
